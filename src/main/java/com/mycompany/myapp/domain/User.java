0cfb00b971ee11d056d87d368ef3702cc4e728c0
frankyoh <frankyoh@tmon.co.kr>
2019-08-08 11:24:23 +0900

    model 파일 수정함

--- src/main/java/com/mycompany/myapp/domain/User.java
+++ src/main/java/com/mycompany/myapp/domain/User.java
@@ -213,18 +213,4 @@ public class User extends AbstractAuditingEntity implements Serializable {
     public int hashCode() {
         return 31;
     }
-
-    @Override
-    public String toString() {
-        return "User{" +
-            "login='" + login + '\'' +
-            ", firstName='" + firstName + '\'' +
-            ", lastName='" + lastName + '\'' +
-            ", email='" + email + '\'' +
-            ", imageUrl='" + imageUrl + '\'' +
-            ", activated='" + activated + '\'' +
-            ", langKey='" + langKey + '\'' +
-            ", activationKey='" + activationKey + '\'' +
-            "}";
-    }
 }
