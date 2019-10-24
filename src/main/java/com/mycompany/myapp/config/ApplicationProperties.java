6611eb30cb961d54c5f8dc424b79a3028c5fda0e
frankyoh <frankyoh@tmon.co.kr>
2019-10-24 13:33:53 +0900

    commit #2입니다.

--- src/main/java/com/mycompany/myapp/config/ApplicationProperties.java
+++ src/main/java/com/mycompany/myapp/config/ApplicationProperties.java
@@ -7,6 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * <p>
  * Properties are configured in the {@code application.yml} file.
  * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
+ * commit #2
  */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
