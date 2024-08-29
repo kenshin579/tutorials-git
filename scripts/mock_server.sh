4afce45a158fbba6bd5cc459c9b4f15919198baf
kenshin579 <kenshin579@hotmail.com>
2024-08-29 18:39:15 +0900

    [#42] http body, status code 출력하도록 수정

--- scripts/mock_server.sh
+++ scripts/mock_server.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 echo "calling mock server"
-response=$(curl --location --request POST 'https://58f4a27e-0002-4bff-98a3-45311947892f.mock.pstmn.io/test' -o /dev/null -w "%{http_code}")
+response=$(curl --write-out "HTTPSTATUS:%{http_code}" --silent --output - --location --request POST "https://58f4a27e-0002-4bff-98a3-45311947892f.mock.pstmn.io/test")
 
-if [[ "$response" -ge 400 ]]; then
-  echo "[ERROR] failed with status code ${response}"
+# Extract the body and the status code
+http_body=$(echo "$response" | sed -e 's/HTTPSTATUS\:.*//g')
+http_status=$(echo "$response" | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+echo "Response Body: $http_body"
+
+if [[ "http_status" -ge 400 ]]; then
+  echo "[ERROR] failed with status code ${http_status}"
   exit 1
 fi
